57714e9cf80d3b28bca2de2726a9f7b064d72363
Subin T P <subinthattaparambil@gmail.com>
2023-12-21 23:46:11 +0530

    Add todo

--- lib/multiwoven/integrations/core/destination_connector.rb
+++ lib/multiwoven/integrations/core/destination_connector.rb
@@ -3,6 +3,7 @@
 module Multiwoven
   module Integrations::Core
     class DestinationConnector < BaseConnector
+      # TODO: Add sync type insert, update, delete as argument
       def write(_sync_config, _records)
         raise "Not implemented"
         # return list of record message
