df4444bd30431ccd1ccf6803e969056634805774
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2023-12-24 15:01:43 +0530

    CI action FIX

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,10 @@
 require "multiwoven/integrations"
 require "webmock/rspec"
 
+require "simplecov"
+require "simplecov_json_formatter"
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
