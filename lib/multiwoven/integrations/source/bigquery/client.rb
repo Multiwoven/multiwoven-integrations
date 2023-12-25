e3cc0de98279e72f75afdc091ab3612965763c4f
Afthab VP <>
2023-12-25 23:14:47 +0530

    spec changes

--- lib/multiwoven/integrations/source/bigquery/client.rb
+++ lib/multiwoven/integrations/source/bigquery/client.rb
@@ -4,10 +4,8 @@ module Multiwoven::Integrations::Source
   module Bigquery
     include Multiwoven::Integrations::Core
     class Client < SourceConnector
-
       def check_connection(connection_config)
         puts connection_config
-        
       end
     end
   end
