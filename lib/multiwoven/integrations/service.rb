2d378022210ff77f956f15e290217c84a6431b78
Subin T P <subinthattaparambil@gmail.com>
2023-12-26 17:15:56 +0530

    Make service class as singleton

--- lib/multiwoven/integrations/service.rb
+++ lib/multiwoven/integrations/service.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true
-
 module Multiwoven
   module Integrations
     class Service
-      def initialize
-        yield(config) if block_given?
-      end
-
-      def connectors
-        {
-          source: build_connectors(ENABLED_SOURCES, "Source"),
-          destination: build_connectors(ENABLED_DESTINATIONS, "Destination")
-        }
-      end
-
-      def connector_class(connector_type, connector_name)
-        Object.const_get("Multiwoven::Integrations::#{connector_type}::#{connector_name}::Client")
-      end
-
-      private
-
-      def build_connectors(enabled_connectors, type)
-        enabled_connectors.map do |connector|
-          connector_class(
-            type, connector
-          ).new.meta_data
+      class << self
+        def initialize
+          yield(config) if block_given?
         end
-      end
 
-      def config
-        @config ||= Config.new
+        def connectors
+          {
+            source: build_connectors(ENABLED_SOURCES, "Source"),
+            destination: build_connectors(ENABLED_DESTINATIONS, "Destination")
+          }
+        end
+
+        def connector_class(connector_type, connector_name)
+          Object.const_get("Multiwoven::Integrations::#{connector_type}::#{connector_name}::Client")
+        end
+
+        private
+
+        def build_connectors(enabled_connectors, type)
+          enabled_connectors.map do |connector|
+            connector_class(
+              type, connector
+            ).new.meta_data
+          end
+        end
+
+        def config
+          @config ||= Config.new
+        end
       end
     end
   end
--- spec/multiwoven/integrations/service_spec.rb
+++ spec/multiwoven/integrations/service_spec.rb
@@ -1,37 +1,29 @@
 # frozen_string_literal: true
 
 RSpec.describe Multiwoven::Integrations::Service do
-  let(:service) { described_class.new }
-
-  describe "#initialize" do
-    it "initializes with a default config" do
-      expect(service.send(:config)).to be_a(Multiwoven::Integrations::Config)
-    end
-
-    it "yields the config to a block if given" do
-      expect { |b| described_class.new(&b) }.to yield_with_args(Multiwoven::Integrations::Config)
+  describe ".initialize" do
+    it "yields with config object" do
+      expect { |b| described_class.initialize(&b) }.to yield_with_args(Multiwoven::Integrations::Config)
     end
   end
 
-  describe "#connectors" do
+  describe ".connectors" do
     before do
-      stub_const("Multiwoven::Integrations::ENABLED_SOURCES", ["TestSource"])
-      stub_const("Multiwoven::Integrations::ENABLED_DESTINATIONS", ["TestDestination"])
-
-      class_double("Multiwoven::Integrations::Source::TestSource::Client").as_stubbed_const
-      class_double("Multiwoven::Integrations::Destination::TestDestination::Client").as_stubbed_const
-
-      allow(Multiwoven::Integrations::Source::TestSource::Client).to receive(:new).and_return(double(meta_data: "source_meta"))
-      allow(Multiwoven::Integrations::Destination::TestDestination::Client).to receive(:new).and_return(double(meta_data: "destination_meta"))
+      stub_const("Multiwoven::Integrations::Service::ENABLED_SOURCES", ["Source1"])
+      stub_const("Multiwoven::Integrations::Service::ENABLED_DESTINATIONS", ["Destination1"])
+      allow(described_class).to receive(:connector_class).and_return(double("Connector", new: double("Instance", meta_data: {})))
     end
 
-    it "returns connectors with source and destination arrays" do
-      expect(service.connectors).to eq(
-        {
-          source: ["source_meta"],
-          destination: ["destination_meta"]
-        }
-      )
+    it "returns a hash with sources and destinations" do
+      expect(described_class.connectors).to have_key(:source)
+      expect(described_class.connectors).to have_key(:destination)
+    end
+  end
+
+  describe ".connector_class" do
+    it "constructs the correct class constant" do
+      connector = described_class.connector_class("Source", "Snowflake")
+      expect(connector).to eq(Multiwoven::Integrations::Source::Snowflake::Client)
     end
   end
 end
