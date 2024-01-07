9a22d197e0b0f5c2b0da6b3bcecdad97e0a7adab
Karthik Sivadas <karthiksivadas@outlook.com>
2024-01-08 00:39:23 +0530

    Rspec for Salesforce CRM destination connector

--- lib/multiwoven/integrations/destination/salesforce_crm/client.rb
+++ lib/multiwoven/integrations/destination/salesforce_crm/client.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'stringio'
+require "stringio"
 
 module Multiwoven
   module Integrations
@@ -8,11 +8,11 @@ module Multiwoven
       module SalesforceCrm
         include Multiwoven::Integrations::Core
 
-        API_VERSION = '59.0'
+        API_VERSION = "59.0"
 
         class Client < DestinationConnector
           def check_connection(connection_config)
-            setup_client(connection_config)
+            initialize_client(connection_config)
             authenticate_client
             success_status
           rescue StandardError => e
@@ -23,35 +23,40 @@ module Multiwoven
             catalog = build_catalog(load_catalog_streams)
             catalog.to_multiwoven_message
           rescue StandardError => e
-            handle_discovery_exception(e)
+            handle_exception("SALESFORCE:CRM:DISCOVER:EXCEPTION", "error", e)
           end
 
-          def write(sync_config, records, action = 'create')
-            setup_client(sync_config[:destination][:connection_specification])
-            process_records(records, sync_config[:streams])
+          def write(sync_config, records, _action = "create")
+            initialize_client(sync_config[:destination][:connection_specification])
+            process_records(records, sync_config[:stream])
           rescue StandardError => e
-            handle_write_exception(e)
+            handle_exception("SALESFORCE:CRM:WRITE:EXCEPTION", "error", e)
           end
 
           private
 
-          def setup_client(connection_config)
-            @client ||= initialize_client(connection_config)
+          def initialize_client(config)
+            @client = Restforce.new(oauth_token: config[:access_token],
+                                    refresh_token: config[:refresh_token],
+                                    instance_url: config[:instance_url],
+                                    client_id: config[:client_id],
+                                    client_secret: config[:client_secret],
+                                    authentication_callback: proc { |x| log_debug(x.to_s) },
+                                    api_version: API_VERSION)
           end
 
           def process_records(records, stream)
-            write_success, write_failure = 0, 0
+            write_success = 0
+            write_failure = 0
             properties = stream[:json_schema][:properties]
-
             records.each do |record_object|
               record = extract_data(record_object, properties)
               process_record(stream, record)
               write_success += 1
             rescue StandardError => e
-              handle_write_exception(e)
+              handle_exception("SALESFORCE:CRM:WRITE:EXCEPTION", "error", e)
               write_failure += 1
             end
-
             tracking_message(write_success, write_failure)
           end
 
@@ -76,40 +81,27 @@ module Multiwoven
             end
           end
 
-          def initialize_client(config)
-            Restforce.new(
-              oauth_token: config[:access_token],
-              refresh_token: config[:refresh_token],
-              instance_url: config[:instance_url],
-              client_id: config[:client_id],
-              client_secret: config[:client_secret],
-              authentication_callback: proc { |x| log_debug(x.to_s) },
-              api_version: API_VERSION
-            )
-          end
-
           def authenticate_client
             @client.authenticate!
           end
 
           def success_status
-            ConnectionStatus.new(status: ConnectionStatusType['succeeded']).to_multiwoven_message
+            ConnectionStatus.new(status: ConnectionStatusType["succeeded"]).to_multiwoven_message
           end
 
           def failure_status(error)
-            ConnectionStatus.new(status: ConnectionStatusType['failed'], message: error.message).to_multiwoven_message
+            ConnectionStatus.new(status: ConnectionStatusType["failed"], message: error.message).to_multiwoven_message
           end
 
           def load_catalog_streams
             catalog_json = read_json(CATALOG_SPEC_PATH)
-            catalog_json['streams'].map { |stream| build_stream(stream) }
+            catalog_json["streams"].map { |stream| build_stream(stream) }
           end
 
           def build_stream(stream)
             Multiwoven::Integrations::Protocol::Stream.new(
-              name: stream['name'],
-              json_schema: stream['json_schema'],
-              action: stream['action']
+              name: stream["name"], json_schema: stream["json_schema"],
+              action: stream["action"]
             )
           end
 
@@ -119,17 +111,8 @@ module Multiwoven
 
           def tracking_message(success, failure)
             Multiwoven::Integrations::Protocol::TrackingMessage.new(
-              success: success,
-              failed: failure
+              success: success, failed: failure
             ).to_multiwoven_message
-          end
-
-          def handle_discovery_exception(exception)
-            handle_exception("SALESFORCE:CRM:DISCOVER:EXCEPTION", "error", exception)
-          end
-
-          def handle_write_exception(exception)
-            handle_exception("SALESFORCE:CRM:WRITE:EXCEPTION", "error", exception)
           end
 
           def log_debug(message)
