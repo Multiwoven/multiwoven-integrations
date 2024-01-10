e308b3913344c00e5bfbe78d3282b721732e50bd
Afthab VP <afthab01gmail.com>
2024-01-10 14:05:29 +0530

    feat: change from sdk to api based

--- lib/multiwoven/integrations/destination/facebook_custom_audiences/client.rb
+++ lib/multiwoven/integrations/destination/facebook_custom_audiences/client.rb
@@ -1,38 +1,77 @@
 # frozen_string_literal: true
 
-require "facebook_ads"
-
 module Multiwoven::Integrations::Destination
   module FacebookCustomAudiences
     include Multiwoven::Integrations::Core
     class Client < DestinationConnector
       def check_connection(connection_config)
-        configure_facebook_ads(connection_config[:access_token])
-        begin
-          ad_accounts = fetch_ad_accounts
-          if ad_account_exists?(ad_accounts, connection_config[:ad_account_id])
-            ConnectionStatus.new(status: ConnectionStatusType["succeeded"]).to_multiwoven_message
-          else
-            ConnectionStatus.new(status: ConnectionStatusType["failed"]).to_multiwoven_message
-          end
-        rescue FacebookAds::ClientError => e
-          ConnectionStatus.new(status: ConnectionStatusType["failed"], message: e.message).to_multiwoven_message
+        access_token = connection_config[:access_token]
+        response = Multiwoven::Integrations::Core::HttpClient.request(
+          FACEBOOK_AUDIENCE_GET_ALL_ACCOUNTS,
+          HTTP_GET,
+          headers: auth_headers(access_token)
+        )
+        if success?(response)
+          ad_account_exists?(response, connection_config[:ad_account_id])
+          ConnectionStatus.new(status: ConnectionStatusType["succeeded"]).to_multiwoven_message
+        else
+          ConnectionStatus.new(status: ConnectionStatusType["failed"]).to_multiwoven_message
         end
+      rescue StandardError => e
+        ConnectionStatus.new(status: ConnectionStatusType["failed"], message: e.message).to_multiwoven_message
       end
 
-      def configure_facebook_ads(access_token)
-        FacebookAds.configure do |config|
-          config.access_token = access_token
+      def discover
+        catalog_json = read_json(CATALOG_SPEC_PATH)
+
+        streams = catalog_json["streams"].map do |stream|
+          Multiwoven::Integrations::Protocol::Stream.new(
+            name: stream["name"],
+            json_schema: stream["json_schema"],
+            url: stream["url"],
+            method: stream["method"],
+            action: stream["action"]
+          )
         end
+
+        catalog = Multiwoven::Integrations::Protocol::Catalog.new(
+          streams: streams
+        )
+
+        catalog.to_multiwoven_message
+      rescue StandardError => e
+        handle_exception(
+          "FACEBOOK AUDIENCE:DISCOVER:EXCEPTION",
+          "error",
+          e
+        )
       end
 
-      def ad_account_exists?(ad_accounts, ad_account_id)
-        ad_accounts.any? { |ad_account| ad_account.id == "act_#{ad_account_id}" }
+      def auth_headers(access_token)
+        {
+          "Accept" => "application/json",
+          "Authorization" => "Bearer #{access_token}",
+          "Content-Type" => "application/json"
+        }
       end
 
-      def fetch_ad_accounts
-        me = FacebookAds::User.get("me")
-        me.adaccounts(fields: "id,name")
+      def ad_account_exists?(response, ad_account_id)
+        return if extract_data(response).any? { |ad_account| ad_account["id"] == formatted_ad_account_id(ad_account_id) }
+
+        raise ArgumentError, "Ad account not found in business account"
+      end
+
+      def success?(response)
+        response && %w[200 201].include?(response.code.to_s)
+      end
+
+      def extract_data(response)
+        response_body = response.body
+        JSON.parse(response_body)["data"] if response_body
+      end
+
+      def formatted_ad_account_id(ad_account_id)
+        "act_#{ad_account_id}"
       end
     end
   end
