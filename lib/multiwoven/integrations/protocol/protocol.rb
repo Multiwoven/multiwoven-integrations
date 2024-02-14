e3508cd3cb700dbacdfd4a92ffc0e96cf1e71a1c
Afthab <afthab01@gmail.com>
2024-02-14 18:02:14 +0530

    [feat]: spec and pr comments resolved

--- lib/multiwoven/integrations/protocol/protocol.rb
+++ lib/multiwoven/integrations/protocol/protocol.rb
@@ -116,8 +116,8 @@ module Multiwoven
       # Applicable for API streams
       attribute? :url, Types::String.optional
       attribute? :request_method, Types::String.optional
-      attribute? :batch_support, Types::Bool.optional
-      attribute? :batch_size, Types::Integer.optional
+      attribute :batch_support, Types::Bool.default(false)
+      attribute :batch_size, Types::Integer.default(1)
     end
 
     class Catalog < ProtocolModel
