e02f89e1d4fa9ea2a7ad4d6208fc7c6f975688c6
Subin T P <subinthattaparambil@gmail.com>
2024-03-05 01:36:37 +0530

    add discover

--- lib/multiwoven/integrations/source/databricks/client.rb
+++ lib/multiwoven/integrations/source/databricks/client.rb
@@ -12,6 +12,29 @@ module Multiwoven::Integrations::Source
         ConnectionStatus.new(status: ConnectionStatusType["failed"], message: e.message).to_multiwoven_message
       end
 
+      def discover(connection_config)
+        connection_config = connection_config.with_indifferent_access
+        query = "SELECT table_name, column_name, data_type, is_nullable
+                FROM system.information_schema.columns
+                WHERE table_schema = \'#{connection_config[:schema]}\' AND table_catalog = \'#{connection_config[:catalog]}\'
+                ORDER BY table_name, ordinal_position;"
+
+        db = create_connection(connection_config)
+
+        records = []
+        db.fetch(query.gsub("\n", "")) do |row|
+          records << row
+        end
+        catalog = Catalog.new(streams: create_streams(records))
+        catalog.to_multiwoven_message
+      rescue StandardError => e
+        handle_exception(
+          "DATABRICKS:DISCOVER:EXCEPTION",
+          "error",
+          e
+        )
+      end
+
       def read(sync_config)
         connection_config = sync_config.source.connection_specification
         connection_config = connection_config.with_indifferent_access
@@ -54,6 +77,21 @@ module Multiwoven::Integrations::Source
         ThriftTransport=2;AuthMech=3;
         AllowSelfSignedServerCert=1;
         CAIssuedCertNamesMismatch=1"
+      end
+
+      def create_streams(records)
+        group_by_table(records).map do |r|
+          Multiwoven::Integrations::Protocol::Stream.new(name: r[:tablename], action: StreamAction["fetch"], json_schema: convert_to_json_schema(r[:columns]))
+        end
+      end
+
+      def group_by_table(records)
+        records.group_by { |entry| entry[:table_name] }.map do |table_name, columns|
+          {
+            tablename: table_name,
+            columns: columns.map { |column| { column_name: column[:column_name], type: column[:data_type], optional: column[:is_nullable] == "YES" } }
+          }
+        end
       end
     end
   end
--- spec/multiwoven/integrations/source/databricks/client_spec.rb
+++ spec/multiwoven/integrations/source/databricks/client_spec.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do
+RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do # rubocop:disable Metrics/BlockLength
   let(:client) { Multiwoven::Integrations::Source::Databricks::Client.new }
 
-  let(:sync_config) do
+  let(:sync_config_json) do
     {
       "source": {
         "name": "databrick-source",
@@ -12,7 +12,9 @@ RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do
           "host": "test-host.databricks.com",
           "http_path": "test_http_path",
           "access_token": "test_toekn",
-          "port": "443"
+          "port": "443",
+          "catalog": "system",
+          "schema": "information_schema"
         }
       },
       "destination": {
@@ -24,7 +26,7 @@ RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do
       },
       "model": {
         "name": "ExampleModel",
-        "query": "SELECT * FROM CALL_CENTER",
+        "query": "SELECT * FROM samples.nyctaxi.trips;",
         "query_type": "raw_sql",
         "primary_key": "id"
       },
@@ -44,12 +46,21 @@ RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do
       "destination_sync_mode": "upsert"
     }
   end
+
+  let(:sync_config) { Multiwoven::Integrations::Protocol::SyncConfig.from_json(sync_config_json.to_json) }
+
+  describe "method definition" do
+    it "defines a private #query method" do
+      expect(described_class.private_instance_methods).to include(:query)
+    end
+  end
+
   describe "#check_connection" do
     context "when the connection is successful" do
       it "returns a succeeded connection status" do
         allow(Sequel).to receive(:odbc).and_return(true)
 
-        result = client.check_connection(sync_config[:source][:connection_specification])
+        result = client.check_connection(sync_config_json[:source][:connection_specification])
         expect(result.type).to eq("connection_status")
 
         connection_status = result.connection_status
@@ -62,13 +73,97 @@ RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do
       it "returns a failed connection status with an error message" do
         allow(Sequel).to receive(:odbc).and_raise(Sequel::DatabaseConnectionError, "Connection failed")
 
-        result = client.check_connection(sync_config[:source][:connection_specification])
+        result = client.check_connection(sync_config_json[:source][:connection_specification])
         expect(result.type).to eq("connection_status")
 
         connection_status = result.connection_status
         expect(connection_status.status).to eq("failed")
         expect(connection_status.message).to eq("Connection failed")
       end
+    end
+  end
+
+  describe "#discover" do
+    it "discover schema successfully" do
+      allow(Sequel).to receive(:odbc).and_return(double("db").as_null_object)
+      allow_any_instance_of(RSpec::Mocks::Double).to receive(:fetch).and_yield(
+        table_name: "TEST_TABLE",
+        column_name: "ID",
+        data_type: "NUMBER",
+        is_nullable: "YES"
+      )
+
+      message = client.discover(sync_config[:source][:connection_specification])
+
+      expect(message.catalog).to be_an(Multiwoven::Integrations::Protocol::Catalog)
+
+      first_stream = message.catalog.streams.first
+      expect(first_stream).to be_a(Multiwoven::Integrations::Protocol::Stream)
+      expect(first_stream.name).to eq("TEST_TABLE")
+      expect(first_stream.json_schema).to be_an(Hash)
+      expect(first_stream.json_schema["type"]).to eq("object")
+      expect(first_stream.json_schema["properties"]).to eq({ "ID" => { "type" => %w[integer null] } })
+    end
+
+    it "discover schema failure" do
+      allow(client).to receive(:create_connection).and_raise(StandardError.new("test error"))
+      expect(client).to receive(:handle_exception).with(
+        "DATABRICKS:DISCOVER:EXCEPTION",
+        "error",
+        an_instance_of(StandardError)
+      )
+      client.discover(sync_config[:source][:connection_specification])
+    end
+  end
+
+  describe "#read" do
+    it "reads records successfully" do
+      allow(Sequel).to receive(:odbc).and_return(double("db").as_null_object)
+
+      allow_any_instance_of(RSpec::Mocks::Double).to receive(:fetch).with(sync_config.model.query).and_yield(id: 1, name: "John").and_yield(id: 2, name: "Jane")
+
+      records = client.read(sync_config)
+
+      expect(records).to be_an(Array)
+      expect(records.length).to eq(2)
+
+      multiwoven_message = records.first
+      first_record = multiwoven_message.record
+      expect(first_record).to be_a(Multiwoven::Integrations::Protocol::RecordMessage)
+      expect(first_record.data).to eq(id: 1, name: "John")
+      expect(first_record.emitted_at).to be_an(Integer)
+    end
+
+    it "reads records successfully for batched_query" do
+      allow(Sequel).to receive(:odbc).and_return(double("db").as_null_object)
+
+      sync_config.limit = "10"
+      sync_config.offset = "1"
+
+      batched_query = client.send(:batched_query, sync_config.model.query, sync_config.limit, sync_config.offset)
+
+      allow_any_instance_of(RSpec::Mocks::Double).to receive(:fetch).with(batched_query).and_yield(id: 1, name: "John").and_yield(id: 2, name: "Jane")
+
+      records = client.read(sync_config)
+
+      expect(records).to be_an(Array)
+      expect(records.length).to eq(2)
+
+      multiwoven_message = records.first
+      first_record = multiwoven_message.record
+      expect(first_record).to be_a(Multiwoven::Integrations::Protocol::RecordMessage)
+      expect(first_record.data).to eq(id: 1, name: "John")
+      expect(first_record.emitted_at).to be_an(Integer)
+    end
+
+    it "read failure" do
+      allow(client).to receive(:create_connection).and_raise(StandardError.new("test error"))
+      expect(client).to receive(:handle_exception).with(
+        "DATABRICKS:READ:EXCEPTION",
+        "error",
+        an_instance_of(StandardError)
+      )
+      client.read(sync_config)
     end
   end
 end
