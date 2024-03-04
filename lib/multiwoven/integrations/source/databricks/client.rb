bfee1f246ce9ea4e17f3e8b7b53911953f00f0a5
Subin T P <subinthattaparambil@gmail.com>
2024-03-04 19:34:43 +0530

    implement check_connection

--- lib/multiwoven/integrations/source/databricks/client.rb
+++ lib/multiwoven/integrations/source/databricks/client.rb
@@ -4,6 +4,32 @@ module Multiwoven::Integrations::Source
   module Databricks
     include Multiwoven::Integrations::Core
     class Client < SourceConnector
+      def check_connection(connection_config)
+        connection_config = connection_config.with_indifferent_access
+        create_connection(connection_config)
+        ConnectionStatus.new(status: ConnectionStatusType["succeeded"]).to_multiwoven_message
+      rescue Sequel::DatabaseConnectionError => e
+        ConnectionStatus.new(status: ConnectionStatusType["failed"], message: e.message).to_multiwoven_message
+      end
+
+      private
+
+      def create_connection(connection_config)
+        Sequel.odbc(drvconnect: generate_drvconnect(connection_config))
+      end
+
+      def generate_drvconnect(connection_config)
+        "Driver=#{DATABRICKS_DRIVER_PATH};
+        Host=#{connection_config[:host]};
+        PORT=#{connection_config[:port]};
+        SSL=1;
+        HTTPPath=#{connection_config[:http_path]};
+        PWD=#{connection_config[:access_token]};
+        UID=token;
+        ThriftTransport=2;AuthMech=3;
+        AllowSelfSignedServerCert=1;
+        CAIssuedCertNamesMismatch=1"
+      end
     end
   end
 end
---
+++ spec/multiwoven/integrations/source/databricks/client_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+RSpec.describe Multiwoven::Integrations::Source::Databricks::Client do
+  let(:client) { Multiwoven::Integrations::Source::Databricks::Client.new }
+
+  let(:sync_config) do
+    {
+      "source": {
+        "name": "databrick-source",
+        "type": "source",
+        "connection_specification": {
+          "host": "test-host.databricks.com",
+          "http_path": "test_http_path",
+          "access_token": "test_toekn",
+          "port": "443"
+        }
+      },
+      "destination": {
+        "name": "DestinationConnectorName",
+        "type": "destination",
+        "connection_specification": {
+          "example_destination_key": "example_destination_value"
+        }
+      },
+      "model": {
+        "name": "ExampleModel",
+        "query": "SELECT * FROM CALL_CENTER",
+        "query_type": "raw_sql",
+        "primary_key": "id"
+      },
+      "stream": {
+        "name": "example_stream", "action": "create",
+        "json_schema": { "field1": "type1" },
+        "supported_sync_modes": %w[full_refresh incremental],
+        "source_defined_cursor": true,
+        "default_cursor_field": ["field1"],
+        "source_defined_primary_key": [["field1"], ["field2"]],
+        "namespace": "exampleNamespace",
+        "url": "https://api.example.com/data",
+        "method": "GET"
+      },
+      "sync_mode": "full_refresh",
+      "cursor_field": "timestamp",
+      "destination_sync_mode": "upsert"
+    }
+  end
+  describe "#check_connection" do
+    context "when the connection is successful" do
+      it "returns a succeeded connection status" do
+        allow(Sequel).to receive(:odbc).and_return(true)
+
+        result = client.check_connection(sync_config[:source][:connection_specification])
+        expect(result.type).to eq("connection_status")
+
+        connection_status = result.connection_status
+        expect(connection_status.status).to eq("succeeded")
+        expect(connection_status.message).to be_nil
+      end
+    end
+  end
+end
