be08969b63cbd92f4149d252759040e62773d757
Subin T P <subinthattaparambil@gmail.com>
2024-03-04 23:52:45 +0530

    add read method

--- lib/multiwoven/integrations/source/databricks/client.rb
+++ lib/multiwoven/integrations/source/databricks/client.rb
@@ -12,7 +12,32 @@ module Multiwoven::Integrations::Source
         ConnectionStatus.new(status: ConnectionStatusType["failed"], message: e.message).to_multiwoven_message
       end
 
+      def read(sync_config)
+        connection_config = sync_config.source.connection_specification
+        connection_config = connection_config.with_indifferent_access
+        query = sync_config.model.query
+        query = batched_query(query, sync_config.limit, sync_config.offset) unless sync_config.limit.nil? && sync_config.offset.nil?
+
+        db = create_connection(connection_config)
+
+        query(db, query)
+      rescue StandardError => e
+        handle_exception(
+          "DATABRICKS:READ:EXCEPTION",
+          "error",
+          e
+        )
+      end
+
       private
+
+      def query(connection, query)
+        records = []
+        connection.fetch(query) do |row|
+          records << RecordMessage.new(data: row, emitted_at: Time.now.to_i).to_multiwoven_message
+        end
+        records
+      end
 
       def create_connection(connection_config)
         Sequel.odbc(drvconnect: generate_drvconnect(connection_config))
